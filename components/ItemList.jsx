10ec9ecd49646014c32f885dacacdb55c69815cd
Jorge Julián Acero Lomelí <julianacero98@gmail.com>
2020-10-07 19:26:08 -0500

    Update completed

--- components/ItemList.jsx
+++ components/ItemList.jsx
@@ -1,27 +1,74 @@
 import gql from 'graphql-tag'
-import { useQuery } from '@apollo/react-hooks'
+import { useMutation, useQuery } from '@apollo/react-hooks'
 
 const ITEMS_QUERY = gql`
-  query {
+  {
     allItems {
       data {
         _id
         name
+        isComplete
       }
     }
   }
 `
-
+const DELETE_ITEM = gql`
+  mutation DeleteItem($id: ID!) {
+    deleteItem(id: $id) {
+      _id
+    }
+  }
+`
+const UPDATE_ITEM = gql`
+  mutation UpdateItem($id: ID!, $isComplete: Boolean!) {
+    updateItem(id: $id, data: { isComplete: $isComplete }) {
+      _id
+      isComplete
+    }
+  }
+`
 export default function ItemList() {
   const { data, loading } = useQuery(ITEMS_QUERY)
+  const [updateItem, { loading: updateLoading }] = useMutation(UPDATE_ITEM)
+  const [deleteItem, { loading: deleteLoading }] = useMutation(DELETE_ITEM, {
+    refetchQueries: [{ query: ITEMS_QUERY }],
+  })
 
   if (loading) return <p>Loading...</p>
-
   return (
     <ul>
-      {data?.allItems.data.map(item => (
-        <li key={item._id}>{item.name}</li>
-      ))}
+      {data.allItems.data.map(item => {
+        const labelStyles = item.isComplete
+          ? { style: { textDecoration: 'line-through' } }
+          : {}
+        return (
+          <li key={item._id}>
+            <span {...labelStyles}>{item.name}</span>
+            <button
+              disabled={deleteLoading || updateLoading}
+              style={{ marginLeft: '10px' }}
+              onClick={e => {
+                e.preventDefault()
+                updateItem({
+                  variables: { id: item._id, isComplete: !item.isComplete },
+                })
+              }}
+            >
+              {item.isComplete ? 'Mark Incomplete' : 'Mark Complete'}
+            </button>
+            <button
+              disabled={deleteLoading || updateLoading}
+              style={{ marginLeft: '10px' }}
+              onClick={e => {
+                e.preventDefault()
+                deleteItem({ variables: { id: item._id } })
+              }}
+            >
+              Remove
+            </button>
+          </li>
+        )
+      })}
     </ul>
   )
 }
